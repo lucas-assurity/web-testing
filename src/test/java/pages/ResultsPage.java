99b5cee3e1fbf53807cb559ca143982bf8811b6b
thomasonl <lucas.thomason@assurity.co.nz>
2021-07-15 10:51:55 +1200

    Refactoring

--- src/test/java/pages/ResultsPage.java
+++ src/test/java/pages/ResultsPage.java
@@ -2,18 +2,31 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ResultsPage {
     private final WebDriver driver;
+
+    @FindBy(id="totalCount")
+    private WebElement totalCount;
+
+    @FindBy(css="#top-of-page > div.site-footer")
+    private WebElement footer;
+
+
     public ResultsPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
         WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#top-of-page > div.site-footer")));
+        wait.until(ExpectedConditions.visibilityOf(totalCount));
+        wait.until(ExpectedConditions.visibilityOf(footer));
     }
 
     public String getTotalCount() {
-        return driver.findElement(By.cssSelector("span#totalCount")).getText();
+        return totalCount.getText();
     }
 }
